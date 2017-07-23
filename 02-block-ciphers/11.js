7f875d0d677c7c7835134eced9e1e5a27d630258
Nicholas Starke <starke.nicholas@gmail.com>
2017-07-23 18:46:18 -0500

    Finishing Set 2 Exercise 11

--- 02-block-ciphers/11.js
+++ 02-block-ciphers/11.js
@@ -1,8 +1,8 @@
 var crypto = require('crypto');
 
-var INPUT = "the quick brown fox jumped over the lazy dog | abcdefghijklmnopqrstuvwxyz | aaaaaaaaaaaaaaaaaaaaaaaaa";
-
-var DEBUG = true;
+var INPUT = process.env.INPUT || "asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf";
+var DEBUG = process.env.DEBUG;
+var BLOCK_SIZE = 16;
 
 function generateCipherText(input){
   var key = crypto.randomBytes(16);
@@ -12,33 +12,29 @@ function generateCipherText(input){
 
   var cbc = crypto.createCipheriv('aes-128-cbc', key, iv);
   var ecb = crypto.createCipheriv('aes-128-ecb', key, '');
-  cbc.setAutoPadding(false);
-  ecb.setAutoPadding(false);
 
   var plaintext = [];
   plaintext.push(randomCount());
   plaintext.push(Buffer.from(input));
   plaintext.push(randomCount());
 
-  input = pad(Buffer.concat(plaintext), 16);
+  input = Buffer.concat(plaintext);
 
   if (DEBUG) console.log('PLAINTEXT: ' + input.toString('base64'));
 
   var result = [];
-
   if (randomBool()) {
     if (DEBUG) console.log('MODE: CBC');
     result.push(cbc.update(input));
     result.push(cbc.final());
-     //result += cbc.final();
   } else {
     if (DEBUG) console.log('MODE: ECB');
     result.push(ecb.update(input));
     result.push(ecb.final());
-    //result += ecb.final();
   }
 
-  return Buffer.concat(result);
+  var output = Buffer.concat(result);
+  return output;
 }
 
 function randomCount(){
@@ -46,16 +42,23 @@ function randomCount(){
   return crypto.randomBytes(count);
 }
 
-function pad(input, length){
-  if (input.length % length === 0){
-    return input;
+function chunk(start, line){
+  var result = [];
+  for (var i = start; i < (line.length + start); i += BLOCK_SIZE){
+    result.push(line.slice(i % line.length, (i + BLOCK_SIZE) % line.length));
+  }
+  return result;
+}
+
+function check(chunks){
+  var count = 0;
+  for (var i = 0; i < chunks.length; i++){
+    for (var j = 0; j < chunks.length; j++){
+      if (i !== j && chunks[i].equals(chunks[j])) count++;
+    }
   }
 
-  var padLength = length - (input.length % length);
-  var result = Buffer.alloc(input.length + padLength);
-  result.fill(padLength);
-  input.copy(result);
-  return result;
+  return count;
 }
 
 function randomBool(){
@@ -63,9 +66,24 @@ function randomBool(){
   return random[0] > 0x80;
 }
 
+function analyze(ciphertext){
+  if (DEBUG) console.log('CIPHERTEXT: ' + ciphertext.toString('base64'));
+  var total = 0;
+  for (var j = 0; j < ciphertext.length; j++){
+    var chunks = chunk(j, ciphertext);
+    var count = check(chunks);
+    if (count > 0) total++;
+  }
+
+  if (total > 0){
+    console.log('ecb mode');
+  } else {
+    console.log('cbc mode');
+  }
+}
+
 for (var i = 0; i < 10; i++){
-  var ciphertext = generateCipherText(INPUT).toString('base64');
-  console.log('CIPHERTEXT: ' + ciphertext);
-  console.log('\n----------------------------------------\n');
+  var ciphertextECB = generateCipherText(INPUT);
+  analyze(ciphertextECB);
 }
 
